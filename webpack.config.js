8fb9df5f646b3c35a54dc6ffb02c70b504016290
Jim <jp_code2013@yahoo.com>
2017-11-18 14:44:44 -0800

    CSS Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,13 @@ module.exports = {
       loader: 'babel-loader', // use babel with webpack, .babelrc to define presets
       test: /\.js$/, // file ends in .js
       exclude: /node_modules/ // don't compile node_modules
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader', // Inject CSS to DOM
+        'css-loader' // CSS JS
+      ]
     }]
   },
   devtool: 'cheap-module-eval-source-map', // Compile and show original lines for errors, console logs, etc.
